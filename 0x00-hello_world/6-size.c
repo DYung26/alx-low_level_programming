04c20d195a75f389651f493fa59b637a69796095
DYung26 <theadstore1407@gmail.com>
2023-10-05 05:33:33 -0700

    6.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,11 +6,9 @@ int sizeofint = sizeof(int);
 int sizeoflongint = sizeof(long);
 int sizeoflonglongint = sizeof(long long);
 int sizeoffloat = sizeof(float);
-int sizeofdouble = sizeof(double);
 printf("Size of a char: %d byte(s)\n", sizeofchar);
 printf("Size of an int: %d byte(s)\n", sizeofint);
 printf("Size of a long int: %d byte(s)\n", sizeoflongint);
 printf("Size of a long long int: %d byte(s)\n", sizeoflonglongint);
 printf("Size of a float: %d byte(s)\n", sizeoffloat);
-printf("Size of a double: %d byte(s)\n", sizeofdouble);
 }
