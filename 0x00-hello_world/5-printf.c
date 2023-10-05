3a0a4b6e0eedabef7544a16a25ba4b38cb358c9a
DYung26 <theadstore1407@gmail.com>
2023-10-05 05:01:17 -0700

    5.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main -This function prints a message to the stdout.
+ * Return: It returns 0, indicating successful execution.
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return(0);
+}
