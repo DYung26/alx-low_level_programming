90878f3398ad34ae308d73a3f37ad07428b70297
DYung26 <theadstore1407@gmail.com>
2023-10-05 04:54:44 -0700

    4.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
-int main(void)
+/**
+ * main - The main function is the entry point of the program.
+ * This function is called when the program is executed.
+ * Return: It prints a message to the standard output and returns 0,
+ * indicating successful execution.
+ */
+void main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
