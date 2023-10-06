ac6f95d26405b68cc69fd84d2573848d4230427d
DYung26 <theadstore1407@gmail.com>
2023-10-06 08:04:52 -0700

    3.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ * Description: This program prints the lowercase alphabet from 'a' to 'z'
+ *              followed by a newline character.
+ * Return: Always returns 0 to indicate successful completion.
+*/
+int main(void)
+{
+	const char *str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		putchar(str[i]);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
