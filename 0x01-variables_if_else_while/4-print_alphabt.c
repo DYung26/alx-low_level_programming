547e1d0dd7762cf1de7e7faf11de1e2e86830f7a
DYung26 <theadstore1407@gmail.com>
2023-10-06 08:22:14 -0700

    4.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,4 +1,10 @@
 #include <stdio.h>
+/**
+ * main - Entry point of the program
+ * Description: This program prints the lowercase alphabet characters
+ *              from 'a' to 'z' followed by a newline character using a while loop.
+ * Return: Always returns 0 to indicate successful completion.
+*/
 int main(void)
 {
 	const char *str = "abcdfghijklmnoprstuvwxyz";
