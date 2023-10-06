6e2f659708c50d87a3fd10a9db4300d31adb1b7f
DYung26 <theadstore1407@gmail.com>
2023-10-06 08:40:56 -0700

    5.0

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * Description: This program demonstrates the use of the printf function
+ *              to print a string containing the digits '0' to '9'.
+ *
+ * Return: Always returns 0 to indicate successful completion.
+ */
+int main(void)
+{
+	const char *str = "0123456789";
+
+	printf("%s", str);
+	return (0);
+}
