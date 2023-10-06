69cb822eebc665ce6880633865ab410ad3495df2
DYung26 <theadstore1407@gmail.com>
2023-10-06 10:47:27 -0700

    9.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -5,13 +5,11 @@ int main(void)
 
 	for(d1 = 0; d1 < 10; d1++)
 	{
+		putchar(d1 + '0');
 		if (d1 == 9)
-		{
-			putchar(d1 + '0');
-		}
+			putchar('\n')
 		else
 		{
-			putchar(d1 + '0');
 			putchar(',');
 		}
 		putchar(' ');
