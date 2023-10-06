e553afa513a7be3ba3f0bb21dbcc81c62f3e0887
DYung26 <theadstore1407@gmail.com>
2023-10-06 10:19:35 -0700

    9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(void)
+{
+	int d1, d2;
+
+	for(d1 = 0; d1 < 10; d1++)
+	{
+		for(d2 = d1 + 1; d2 < 10; d2++)
+		{
+			putchar(d1 + '0');
+			putchar(d2 + '0');
+			putchar(',');
+			putchar(' ');
+		}
+	}
+}
