e72a2ec2e452b8d8376b9debb5cefda2a4f312d7
DYung26 <theadstore1407@gmail.com>
2023-10-06 10:55:12 -0700

    9.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,4 +1,14 @@
 #include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * Description: This program prints all possible combinations of single-digit
+ *              numbers in ascending order, separated by commas and spaces.
+ *              Each combination is followed by a newline character, except
+ *              for the last one.
+ *
+ * Return: Always returns 0 to indicate successful completion.
+ */
 int main(void)
 {
 	int d1;
