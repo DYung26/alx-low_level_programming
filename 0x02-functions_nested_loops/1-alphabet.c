ee83903a25a80edba7e8ca6b118d0a791912fea9
DYung26 <theadstore1407@gmail.com>
2023-10-10 11:27:45 -0700

    #include <1.h>.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
-/*#include "main.h"*/
+/**
+ * main - Entry point of the program
+ * This is the starting point of the program. It calls the print_alphabet
+ * function to print the lowercase alphabet characters
+ * to the standard output.
+ * Return: Always 0 (Success)
+ * The function returns 0 to indicate successful execution.
+ */
 void print_alphabet(void)
 {
 	const char *alphbtlwrcs = "abcdefghijklmnopqrstuvwxyz";
