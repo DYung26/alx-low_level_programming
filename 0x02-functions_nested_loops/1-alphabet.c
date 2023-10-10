3fac1a49861dea8e0a44ed04a2f60f539f988325
DYung26 <theadstore1407@gmail.com>
2023-10-10 04:17:49 -0700

    #include <1.h>.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,7 +5,7 @@ void print_alphabet(void)
 	const char *alphbtlwrcs = "abcdefghijklmnopqrstuvwxyz";
 	int i = 0;
 
-	while (alphbtlwrcs[i] != '\0');
+	while (alphbtlwrcs[i] != '\0')
 	{
 		putchar(alphbtlwrcs[i]);
 		i++;
