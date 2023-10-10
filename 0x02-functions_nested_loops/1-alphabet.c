2c60a9309a4d891332de7f01e4efa82a2f8f88ec
DYung26 <theadstore1407@gmail.com>
2023-10-10 11:35:13 -0700

    #include <1.h>.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - Entry point of the program
+ * print_alphabet - Entry point of the program
  * This is the starting point of the program. It calls the print_alphabet
  * function to print the lowercase alphabet characters
  * to the standard output.
