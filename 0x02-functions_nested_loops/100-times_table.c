424d4a5e46b658d4834d937e637237a05bfd6358
DYung26 <theadstore1407@gmail.com>
2023-10-10 23:42:18 -0700

    #include <12.h>.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,8 +10,8 @@ void print_times_table(int n)
 			_putchar('0');
 			for (mult = 1; mult <= n; mult++)
 			{
-				_putchar(',')
-				_putchar(' ')
+				_putchar(',');
+				_putchar(' ');
 				prod = num + mult
 				if (prod <= 99)
 					_putchar(' ');
@@ -28,7 +28,7 @@ void print_times_table(int n)
 				}
 				_putchar((prod % 10) + '0');
 			}
-			_putchar('\n')
+			_putchar('\n');
 		}
 	}
 }
