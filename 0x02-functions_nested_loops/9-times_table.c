16ec991af4208851680fe3ccc9b50ed65cb12a0b
DYung26 <theadstore1407@gmail.com>
2023-10-10 22:18:25 -0700

    #include <9.h>.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,12 +12,10 @@ void times_table(void)
 		for (y = 0; y <= 9; y++)
 		{
 			z = x * y;
-			
 			if (z < 9)
 			{
 				u = z % 10;
 				d = (z - u) / 10;
-
 				_putchar(44);
 				_putchar(32);
 				_putchar(d + '0');
