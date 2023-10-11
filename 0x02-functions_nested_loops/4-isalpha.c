f1cf719252e8668287ed382d210923a58209cb23
DYung26 <theadstore1407@gmail.com>
2023-10-10 20:30:17 -0700

    #include <4.h>.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * _islower - checks if a character is lowercase
+ * @c: the character to check
+ * Return: 1 if c is a lowercase letter, 0 otherwise
+ */
+int main(void)
+{
+	if ((c >= 65 && c <= 97) || (c >= 97 && c <= 122))
+		return (1);
+	return (0);
+}
