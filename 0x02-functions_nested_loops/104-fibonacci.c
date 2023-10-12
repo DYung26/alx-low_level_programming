fe58a615a3855cd01ed4f986b68a5c5302db3be7
DYung26 <theadstore1407@gmail.com>
2023-10-12 05:07:00 -0700

    #include <16.h>.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -6,6 +6,7 @@
  * - result: an array to store the result of the addition
  * - num1: the first number as a string
  * - num2: the second number as a string
+ * - str: Get the length of the string.
  */
 void reverseString(char str[]);
 
@@ -38,7 +39,7 @@ void addStrings(char result[], char num1[], char num2[])
 }
 
 /**
- * Function to reverse a string.
+ * reverseString - Function to reverse a string.
  * Parameter:
  * - str: the string to be reversed.
  */
