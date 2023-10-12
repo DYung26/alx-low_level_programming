f6254913e7bbfbfe95aabbd71238d9ffc5c3d5cd
DYung26 <theadstore1407@gmail.com>
2023-10-11 19:08:51 -0700

    #include <16.h>.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -34,13 +34,11 @@ int main(void)
 		}
 		printf("%lu%lu", half1, half2);
 		if (count != 98)
-		{
 			printf(", ");
-			fib1_half1 = fib2_half1;
-			fib1_half2 = fib2_half2;
-			fib2_half1 = half1;
-			fib2_half2 = half2;
-		}
+		fib1_half1 = fib2_half1;
+		fib1_half2 = fib2_half2;
+		fib2_half1 = half1;
+		fib2_half2 = half2;
 	}
 	printf("\n");
 	return (0);
