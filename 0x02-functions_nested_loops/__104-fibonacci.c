b718b1f7e18226368ef1f7c23a323690a40a2065
DYung26 <theadstore1407@gmail.com>
2023-10-11 21:59:20 -0700

    #include <16.h>.c

---
+++ 0x02-functions_nested_loops/__104-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Entry point for the program
+ * Description: Generates and prints the first 98 Fibonacci numbers.
+ * The numbers are separated by commas and followed by a new line.
+ * Return: Always 0 to indicate success
+ */
+int main(void)
+{
+	int i = 0;
+	unsigned long int a = 0, b = 1, next = 0;
+
+	while (i < 98)
+	{
+		next = a + b;
+		a = b;
+		b = next;
+		printf("%lu", next);
+
+		if (i < 97)
+			printf(", ");
+		i++;
+	}
+	putchar("\n");
+	return (0);
+}
+
