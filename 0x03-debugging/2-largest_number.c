9f4fa78a391f25b63617d905ab3a5c9955e1ec92
DYung26 <theadstore1407@gmail.com>
2023-10-12 17:34:53 -0700

    #include <2.h>.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,20 +10,23 @@
 
 int largest_number(int a, int b, int c)
 {
-    int largest;
+	int largest;
 
-    if (a > b && b > c)
-    {
-        largest = a;
-    }
-    else if (b > a && a > c)
-    {
-        largest = b;
-    }
-    else
-    {
-        largest = c;
-    }
+	if (a > b && a > c)
+	{
+		largest = a;
+		printf("%d is the largest number", a);
+	}
+	else if (b > a && b > c)
+	{
+		largest = b;
+		printf("%d is the largest number", b);
+	}
+	else
+	{
+		largest = c;
+		printf("%d is the largest number", c);
+	}
 
-    return (largest);
+	return (largest);
 }
