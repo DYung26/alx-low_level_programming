8bc032e1b6f782d6a76f6799526f5d2fca9266f2
DYung26 <theadstore1407@gmail.com>
2023-10-12 19:17:25 -0700

    #include <7.h>.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -22,11 +22,9 @@ void print_diagonal(int n)
 		{
 			for (j = 0; j < n; j++)
 			{
-				if (j == 1)
-					_putchar('\\')
-				else if(j < i)
-					_putchar(' ')
+				_putchar(' ');
 			}
+			_putchar('\\');
 			_putchar('\n');
 		}
 	}
