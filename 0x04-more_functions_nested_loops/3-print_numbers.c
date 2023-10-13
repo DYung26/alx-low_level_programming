c1abcbda11155d1539a6e6a639aa71ed6c2c6932
DYung26 <theadstore1407@gmail.com>
2023-10-12 18:53:10 -0700

    #include <3.h>.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers - prints the digits from 0 to 9
+ * Return: None (void)
+ */
+void print_numbers(void)
+{
+	int c;
+
+	for (c = 48; c < 58; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
