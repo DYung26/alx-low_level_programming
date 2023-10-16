e41449dc7ed633e4bf7ae3c7cfae9e2a44fcccd1
DYung26 <theadstore1407@gmail.com>
2023-10-16 14:49:31 -0700

    #include <3.h>.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,4 +1,12 @@
 #include "main.h"
+/**
+ * _puts - Prints a string followed by a newline to standard output.
+ * @str: A pointer to the null-terminated string to be printed.
+ * This function prints each character in the provided string 'str' until
+ * the null terminator '\0' is encountered.
+ * It then appends a newline character to the output.
+ * @str: The null-terminated string to be printed.
+ */
 void _puts(char *str)
 {
 	while (*str != '\0')
