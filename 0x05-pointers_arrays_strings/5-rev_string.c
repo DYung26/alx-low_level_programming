eb75e3702ac14709b5d26c9bbd40c46fa3bd4050
DYung26 <theadstore1407@gmail.com>
2023-10-16 20:22:04 -0700

    #include <5.h>.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,28 +1,22 @@
 #include "main.h"
 /**
- * rev_string - Reverses the characters in a null-terminated string.
- * @s: A pointer to the null-terminated string to be reversed in-place.
- * This function takes a null-terminated string 's'
- * and reverses the order of its characters in-place,
- * meaning it modifies the original string.
- * @s: The null-terminated string to be reversed.
+ * rev_string - Reverses a string
+ * @s: Input string
+ * Return: String in reverse
  */
 void rev_string(char *s)
 {
-	int length = 0;
-	int start = 0;
-	int end = length - 1;
+	char rev = s[0];
+	int counter = 0;
+	int i;
 
-	while (s[length] != '\0')
-		length++;	
-
-	while (start < end)
+	while (s[counter] != '\0')
+	counter++;
+	for (i = 0; i < counter; i++)
 	{
-		char temp = s[start];
-		s[start] = s[end];
-		s[end] = temp;
-
-		start++;
-		end--;
+		counter--;
+		rev = s[i];
+		s[i] = s[counter];
+		s[counter] = rev;
 	}
 }
