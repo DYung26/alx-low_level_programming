06dfb1ff2b83aa84992f150eace42e73c06a9c67
DYung26 <theadstore1407@gmail.com>
2023-10-16 19:45:40 -0700

    #include <4.h>.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,9 +1,28 @@
 #include "main.h"
+/**
+ * print_rev - Prints the reverse of a null-terminated string
+ * followed by a newline.
+ * @s: A pointer to the null-terminated string to be reversed and printed.
+ * This function calculates the length of the provided string 's'
+ * and then prints its characters in reverse order,
+ * followed by a newline character.
+ * @s: The null-terminated string to be reversed and printed.
+ */
 void print_rev(char *s)
 {
+	int i = 0;
+	int o;
+
 	while (*s != '\0')
 	{
-		_putchar(*s--);
+		i++;
+		s++;
 	}
-	_putchar('\n');
+	s--;
+	for (o = i; o > 0; o--)
+	{
+		_putchar(*s);
+		s--;
+	}
+		_putchar('\n');
 }
