def4c4cf078553c6648b5009b21f7501376ebcf8
DYung26 <theadstore1407@gmail.com>
2023-10-25 07:18:20 -0700

    #include <0.h>.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,12 +2,10 @@
 
 void _puts_recursion(char *s)
 {
-	int i = 0;
-
-	if (i < 0)
+	if (*s == '\0')
 	{
 		return;
 	}
-	_putchar(s[i]);
-	_puts_recursion(char *s[i + 1]);
+	_putchar(*s);
+	_puts_recursion(s + 1);
 }
