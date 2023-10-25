2d4611f6c2766595774686dc0f96a840afa93e9b
DYung26 <theadstore1407@gmail.com>
2023-10-25 07:57:51 -0700

    #include <0.h>.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,5 +1,12 @@
 #include "main.h"
-
+/**
+ * _puts_recursion - Prints a string recursively followed by a newline.
+ * @s: The input string to print.
+ *
+ * Description: This function recursively prints each character of the input
+ * string, followed by a newline character when it reaches the end of the
+ * string.
+ */
 void _puts_recursion(char *s)
 {
 	if (s == NULL)
