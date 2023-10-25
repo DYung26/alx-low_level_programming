ffe691b23be933e2b54949698f03683ff6df1b0a
DYung26 <theadstore1407@gmail.com>
2023-10-25 07:42:30 -0700

    #include <0.h>.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,13 +2,14 @@
 
 void _puts_recursion(char *s)
 {
-	if (*s == NULL)
+	if (s == NULL)
 	{
 		return;
 	}
 	if (*s == '\0')
 	{
 		_putchar('\n');
+		return;
 	}
 	_putchar(*s);
 	_puts_recursion(s + 1);
