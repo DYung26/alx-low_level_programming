652d9b43997e258cff1f559b7b70ab2fba63324b
DYung26 <theadstore1407@gmail.com>
2023-10-27 17:23:01 -0700

    #include <0.h>.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,13 @@
 #include "main.h"
+/**
+ * main - The main function of the program.
+ * @argc: The number of command-line arguments.
+ * @argv: An array of strings representing the command-line arguments.
+ *
+ * Return: 0 upon successful execution, an error code otherwise.
+ */
 int main(int argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
+	return (0);
 }
