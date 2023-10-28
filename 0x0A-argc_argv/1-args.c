4d9a6b41392eaad099e6278734ddc218958883c7
DYung26 <theadstore1407@gmail.com>
2023-10-27 18:06:59 -0700

    #include <1.h>.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,4 +1,11 @@
 #include "main.h"
+/**
+ * main - Entry point of the program.
+ * @argc: The number of command-line arguments.
+ * @argv: An array of strings representing the command-line arguments.
+ *
+ * Return: 0 upon successful execution.
+ */
 int main(int argc, char *argv[])
 {
 	if (argc > 0)
