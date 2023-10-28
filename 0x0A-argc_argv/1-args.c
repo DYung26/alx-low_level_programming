bb9408607dad63ec3507539d2a7abf3029ece1b3
DYung26 <theadstore1407@gmail.com>
2023-10-27 17:57:39 -0700

    #include <1.h>.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -3,7 +3,10 @@ int main(int argc, char *argv[])
 {
 	if (argc > 0)
 	{
-		printf("%d\n", sizeof(argv)/sizeof(argv[0]));
+		int len;
+
+		len = sizeof(argv) / sizeof(argv[0])
+		printf("%d\n", len);
 	}
 	printf("%d\n", argc);
 	return (0);
