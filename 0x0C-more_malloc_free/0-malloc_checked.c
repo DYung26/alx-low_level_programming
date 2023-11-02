dbad50ccac83c40f6085c1d44ede5f6209a576a7
DYung26 <theadstore1407@gmail.com>
2023-11-02 10:20:29 -0700

    #include <0.h>.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,5 +1,15 @@
 #include "main.h"
-
+/**
+ * malloc_checked - Allocates memory using malloc
+ * @b: The amount of memory to allocate
+ *
+ * This function allocates a block of memory of size 'b' bytes using the
+ * malloc function. If the allocation fails (i.e., if malloc returns NULL),
+ * the function exits with an error code of 98.
+ *
+ * Return: Upon success, a pointer to the allocated memory block. If the
+ * allocation fails, the function does not return; it exits the program.
+ */
 void *malloc_checked(unsigned int b)
 {
 	int *t = malloc(b);
