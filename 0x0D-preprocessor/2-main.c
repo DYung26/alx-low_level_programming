9a19f25dc110ed27ac0746a6689aa979a98b3ac8
DYung26 <theadstore1407@gmail.com>
2023-11-06 14:49:01 -0800

    #include <2.h>

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
-
+/**
+ * main - The main function of the program.
+ * 
+ * This function prints the name of the current source file
+ * using the __FILE__ macro.
+ * 
+ * Return: Always returns 0 to indicate successful execution.
+ */
 int main(void)
 {
 	printf("%s\n", __FILE__);
