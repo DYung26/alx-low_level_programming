0c1a640b4239113b731ecad331b4682199a06754
DYung26 <theadstore1407@gmail.com>
2023-11-08 03:49:43 -0800

    #include <1.h>.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -9,9 +9,9 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	long unsigned int i = 0;
+	int i = 0;
 
-	while (i < size)
+	while (size_t(i) < size)
 	{
 		action(array[i]);
 		i++;
