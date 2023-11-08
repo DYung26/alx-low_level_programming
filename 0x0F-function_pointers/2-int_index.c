8360a9b432d690a88d97e8f14c038866fe4d89ba
DYung26 <theadstore1407@gmail.com>
2023-11-08 04:59:21 -0800

    #include <2.h>.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -16,18 +16,15 @@
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i = 0;
-	int a = 0;
 
 	if (size <= 0)
 		return (-1);
 	while (i < size)
 	{
-		while (a == 0)
-		{
-			a = cmp(array[i]);
-			i++;
-		}
-		return (i - 1);
+		int a = cmp(array[i]);
+		if (a != 0)
+			return (i);
+		i++;
 	}
 	return (-1);
 }
