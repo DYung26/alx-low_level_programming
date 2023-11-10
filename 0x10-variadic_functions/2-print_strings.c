1f42f2266ea6782fdbc535b76e3dc4a5a5c84744
DYung26 <theadstore1407@gmail.com>
2023-11-09 16:45:07 -0800

    #include <2.h>.c

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -15,6 +15,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	va_list strings;
 	char *str;
 	unsigned int index;
+
 	va_start(strings, n);
 
 	for (index = 0; index < n; index++)
