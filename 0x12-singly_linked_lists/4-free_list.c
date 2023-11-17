2172f27820b16bcd4c4fb773b122c255813030c7
DYung26 <theadstore1407@gmail.com>
2023-11-16 19:44:42 -0800

    #include <>.c

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -11,9 +11,9 @@ void free_list(list_t *head)
 
 	while (head)
 	{
-	temp = head->next;
-	free(head->str);
-	free(head);
-	head = temp;
+		temp = head->next;
+		free(head->str);
+		free(head);
+		head = temp;
 	}
 }
